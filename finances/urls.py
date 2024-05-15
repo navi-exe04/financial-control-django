18b95359502d8892dbdf3cfa6adc4880e23292d9
Raul Langle <ra04navi@outlook.com>
2024-05-15 16:22:47 -0600

    feat: the services for login, register and logout are ready to use

--- finances/urls.py
+++ finances/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name="inicio"),
-    path('gastos/', views.gastos, name="gastos"),
-    path('ingresos/', views.ingresos, name="ingresos"),
-    path('reporte/', views.reporte, name="reporte"),
+    path('', views.home, name="home"),
+    path('expenses/', views.expenses, name="expenses"),
+    path('incomes/', views.incomes, name="incomes"),
+    path('report/', views.report, name="report"),
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register")
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,34 +1,117 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
-def inicio(request):
+def home(request):
     """
-    El inicio del sistema
+    The main view of the system
     """
-    return render(request, 'inicio.html')
+    return render(request, 'home.html')
 
 
-def gastos(request):
+@login_required
+def expenses(request):
     """
-    Vista de gastos del sistema. 
-    Enlista todos los gastos hechos por el usuario.
+    List of expenses of the user
     """
-    return render(request, 'gastos.html')
+    return render(request, 'expenses.html')
 
 
-def ingresos(request):
+@login_required
+def incomes(request):
     """
-    Vista de ingresos del sistema. 
-    Enlista todos los ingresos que ha tenido el usuario.
+    List of incomes of the user
     """
-    return render(request, 'ingresos.html')
+    return render(request, 'incomes.html')
 
 
-def reporte(request):
+@login_required
+def report(request):
     """
-    Vista de reporte de ingresos y gastos.
+    General report of expenses and incomes
     """
-    return render(request, 'reporte.html')
+    return render(request, 'report.html')
+
+
+# user views
+def register_user(request):
+    """
+    GET - Shows the register form to the user
+    POST - Register a new user in system
+    """
+    # GET METHOD
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    # POST METHOD
+    else:
+        error = ""
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        # The passwords match
+        if password1 == password2:
+            # Create a new user in db
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password1
+                )
+                user.save()
+                login(request, user)
+                return redirect('report')
+            # The user already exists in db
+            except IntegrityError:
+                error = "The user already exists"
+        # Passwords do not match
+        else:
+            error = "Passwords do not match"
+
+        return render(request, 'register.html', {
+            'error': error
+        })
+
+
+def login_user(request):
+    """
+    GET - Shows the login form to the user
+    POST - Login in to system
+    """
+    # GET METHOD
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    # POST METHOD
+    else:
+        error = ""
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # verify if the user exists in db
+        user = authenticate(request, username=username, password=password)
+
+        # user is valid
+        if user is not None:
+            # login system
+            login(request, user)
+            return redirect('report')
+        # user is not valid
+        else:
+            error = "Username or password are incorrect"
+            return render(request, 'login.html', {
+                'error': error
+            })
+
+
+@login_required
+def logout_user(request):
+    """
+    Logout the user from the system
+    """
+    logout(request)
+    return redirect('home')
